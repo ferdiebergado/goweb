36c9228d7f1a7f344a88ca540abad5783e97357e
ferdie bergado <ferdiebergado@gmail.com>
2025-02-02 21:04:29 +0800

    Complete signal handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
 )
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
 		slog.Error("Fatal error.", "reason", err)
 		os.Exit(1)
 	}
 }
 
 func run(ctx context.Context) error {
-	_, cancel := signal.NotifyContext(ctx, os.Interrupt)
-	defer cancel()
+	signalCtx, stop := signal.NotifyContext(ctx, os.Interrupt)
+	defer stop()
+
+	fmt.Println("App started. Press Ctrl-C to exit.")
+	<-signalCtx.Done()
 
 	return nil
 }
