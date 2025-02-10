89dca1afcb210db08d9b2f508ec0d31d4d5a777b
ferdie bergado <ferdiebergado@gmail.com>
2025-02-10 21:25:48 +0800

    Rename to base_api

---
+++ internal/handler/base_api.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/ferdiebergado/gopherkit/http/response"
+	"github.com/ferdiebergado/goweb/internal/service"
+)
+
+type BaseHandler struct {
+	service service.Service
+}
+
+func NewBaseHandler(service service.Service) *BaseHandler {
+	return &BaseHandler{service: service}
+}
+
+func (h *BaseHandler) HandleHealth(w http.ResponseWriter, r *http.Request) {
+	status := http.StatusOK
+	msg := "healthy"
+
+	if err := h.service.PingDB(r.Context()); err != nil {
+		status = http.StatusServiceUnavailable
+		msg = "unhealthy"
+		slog.Error("failed to connect to the database", "reason", err)
+	}
+
+	response.JSON(w, r, status, APIResponse[any]{Message: msg})
+}
