201174d435d2d7cba41dbe44adcbbccfc70687dc
ferdie bergado <ferdiebergado@gmail.com>
2025-02-11 09:56:12 +0000

    Created badRequestError helper

--- internal/handler/api.go
+++ internal/handler/api.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"fmt"
+	"log/slog"
 	"net/http"
 
 	"github.com/ferdiebergado/gopherkit/http/response"
 	"github.com/go-playground/validator/v10"
 )
+
+const jsonCT = "application/json"
 
 type validationErrors map[string][]string
 
@@ -28,4 +32,18 @@ func validationError(w http.ResponseWriter, r *http.Request, err error) {
 	}
 
 	response.JSON(w, r, http.StatusBadRequest, res)
+}
+
+func badRequestError(w http.ResponseWriter, r *http.Request, err error) {
+	const status = http.StatusBadRequest
+	slog.Error("request error", "reason", err, "request", fmt.Sprint(r))
+
+	if r.Header.Get("Content-Type") == jsonCT {
+		res := APIResponse[any]{
+			Message: "Invalid input",
+		}
+		response.JSON(w, r, http.StatusBadRequest, res)
+		return
+	}
+	http.Error(w, http.StatusText(status), status)
 }
