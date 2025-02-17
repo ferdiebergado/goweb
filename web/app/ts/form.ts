1f874b862c4533ff271476dd1026c1f141087e93
ferdie bergado <ferdiebergado@gmail.com>
2025-02-17 21:35:17 +0800

    Created form handler

---
+++ web/app/ts/form.ts
@@ -0,0 +1,23 @@
+const form = document.getElementById('x-form') as HTMLFormElement;
+
+form.addEventListener('click', async (e) => {
+  e.preventDefault();
+  try {
+    const frmData = new FormData(form);
+    const obj = Object.fromEntries(frmData);
+    const opts: RequestInit = {
+      method: form.method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    };
+    const res = await fetch(form.action, opts);
+    if (res.ok) {
+      const data = await res.json();
+      console.dir(data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
