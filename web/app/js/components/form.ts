1ef8ff86166f7e1a8a7224d91de02809da72a83d
ferdie bergado <ferdiebergado@gmail.com>
2025-03-03 12:01:16 +0800

    Fixed form reusability

--- web/app/js/components/form.ts
+++ web/app/js/components/form.ts
@@ -1,90 +1,53 @@
-import { APIResponse } from '../types';
+import { APIResponse } from '../@types/api';
+import { FormErrors, FormOptions, FormValues } from '../@types/form';
 
-type ValidateFn = () => FormErrors;
-type HandleFn = <T>(data: T) => undefined;
-type OnErrorFn = (error: unknown) => undefined;
-type FinallyFn = () => undefined;
-type Method = 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+export default function <T extends FormValues, E extends FormErrors>(
+  opts: FormOptions<T, E>
+) {
+  const { data, method, submitUrl, errors, validateFn, onSuccess, onError } =
+    opts;
 
-interface FormData {
-  [key: string]: string;
-}
+  return {
+    data,
+    method: method ?? 'POST',
+    submitUrl,
+    isSubmitting: false,
+    errors,
+    validate() {
+      this.errors = validateFn.call(this) as E;
+      return Object.keys(this.errors).length === 0;
+    },
+    async submit() {
+      if (!this.validate()) return;
 
-export interface FormErrors {
-  [key: string]: string;
-}
+      this.isSubmitting = true;
 
-export interface FormOptions {
-  data: FormData;
-  method?: Method;
-  submitUrl: string;
-  validate: ValidateFn;
-  handle: HandleFn;
-  onError: OnErrorFn;
-  onFinal: FinallyFn;
-}
+      try {
+        const response = await fetch(this.submitUrl, {
+          method: this.method,
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(this.data),
+        });
 
-export const form = (options: FormOptions) => ({
-  data: options.data || [],
-  method: options.method ?? 'POST',
-  submitUrl: options.submitUrl || '',
-  validate: options.validate,
-  handle: options.handle,
-  errorFn: options.onError,
-  finalFn: options.onFinal,
-  errors: {} as FormErrors,
-  isSubmitting: false,
-  submitted: false,
-  submissionError: false,
-
-  validateForm(): boolean {
-    this.errors = this.validate();
-    return Object.keys(this.errors).length === 0;
-  },
-
-  async submitForm(): Promise<undefined> {
-    if (!this.validateForm()) {
-      console.log('invalid input', this.errors);
-
-      return;
-    }
-
-    this.isSubmitting = true;
-    this.submissionError = false;
-
-    try {
-      const res = await fetch(this.submitUrl, {
-        method: this.method,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(this.data),
-      });
-
-      if (!res.ok) {
-        if (res.status in [400, 422]) {
-          const data: APIResponse<undefined> = await res.json();
-          this.errors = data.errors;
-          return;
+        if (!response.ok) {
+          if (response.status in [400, 422]) {
+            const data: APIResponse<undefined> = await response.json();
+            const { message, errors } = data;
+            console.error(message);
+            this.errors = errors as E;
+            return;
+          }
+          throw new Error('Invalid credentials');
         }
-        throw new Error('Network response was not ok');
+
+        const data = await response.json();
+        onSuccess(data);
+      } catch (error) {
+        console.error(error);
+        onError(error);
+      } finally {
+        this.isSubmitting = false;
       }
-
-      const data = await res.json();
-
-      console.log('Success:', data);
-      if (this.handle) this.handle(data);
-
-      this.submitted = true;
-    } catch (error) {
-      console.error(error);
-      if (this.errorFn) this.errorFn(error);
-      this.submissionError = true;
-    } finally {
-      if (this.finalFn) this.finalFn();
-      this.isSubmitting = false;
-    }
-
-    console.log('Form Data:', this.data);
-  },
-});
+    },
+  };
+}
