5f76b4732125247accad04918c875ab3f4bb83df
ferdie bergado <ferdiebergado@gmail.com>
2025-03-15 11:42:48 +0800

    Lint css with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import css from "@eslint/css";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -8,4 +9,17 @@ export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	{
+		files: ["./web/app/css/**/*.css"],
+		plugins: {
+			css,
+		},
+		language: "css/css",
+		languageOptions: {
+			tolerant: true,
+		},
+		rules: {
+			"css/no-empty-blocks": "error",
+		},
+	},
 ];
